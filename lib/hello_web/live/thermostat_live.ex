4a5c2d5b944e2dbce615481a0cb7456ffe2122c3
sisu6828 <148442550+sisu6828@users.noreply.github.com>
2025-04-16 17:16:16 +0200

    Refactor ThermostatLive module and add HTTPoison dependency

--- lib/hello_web/live/thermostat_live.ex
+++ lib/hello_web/live/thermostat_live.ex
@@ -1,39 +1,56 @@
 defmodule HelloWeb.ThermostatLive do
   use HelloWeb, :live_view
+ 
 
   def render(assigns) do
     ~H"""
-    Current temperature: {@temperature} °C
-    <button phx-click="inc_temperature">+</button>
+    Current temperature: {@temperature} °C <button phx-click="inc_temperature">+</button>
     <button phx-click="dec_temperature">-</button>
 
     <button phx-click="nice">69</button>
 
     <button phx-click="random">random</button>
+
+    <p>How about this: {@request}</p>
+    <button phx-click="request">request</button>
     """
   end
 
   def mount(_params, _session, socket) do
-      temperature = 70
-      {:ok, assign(socket, :temperature, temperature)}
+    socket =
+      socket
+      |> assign(:temperature, 70)
+      |> assign(:request, "RARAREQUEST")
+
+    {:ok, socket}
   end
 
-  def handle_event("inc_temperature", _params, socket) do
-      {:noreply, update(socket, :temperature, &(&1 + 1))}
+  def handle_event(event, _params, socket) do
+    case event do
+      "inc_temperature" ->
+        {:noreply, update(socket, :temperature, &(&1 + 1))}
+
+      "dec_temperature" ->
+        {:noreply, update(socket, :temperature, &(&1 - 1))}
+
+      "nice" ->
+        {:noreply, update(socket, :temperature, fn _ -> 69 end)}
+
+      "random" ->
+        {:noreply, update(socket, :temperature, fn _ -> :rand.uniform(100) end)}
+
+      "request" ->
+        case HTTPoison.get("http://www.google.com") do
+          {:ok, %HTTPoison.Response{body: body}} ->
+            {:noreply, assign(socket, :request, body)}
+
+          {:error, %HTTPoison.Error{reason: reason}} ->
+            {:noreply, assign(socket, :request, "Error: #{reason}")}
+        end
+
+      _ ->
+        # Default case for unhandled events
+        {:noreply, socket}
+    end
   end
-
-
-  def handle_event("dec_temperature", _params, socket) do
-      {:noreply, update(socket, :temperature, &(&1 - 1))}
-  end
-
-
-  def handle_event("nice", _params, socket) do
-      {:noreply, update(socket, :temperature, &(&1-&1 +69))}
-  end
-
-  def handle_event("random", _params, socket) do
-      {:noreply, update(socket, :temperature, &(&1-&1 + :rand.uniform(100)))}
-  end
-
 end
